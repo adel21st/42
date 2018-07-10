6265ae57e6b29006f2d592d41d019ef904823de0
Abdeljalil OURIKA <aourika@e2r1p1.42.fr>
2018-07-10 18:42:13 +0200

    j02

---
+++ ex05/ft_print_comb2.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aourika <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/10 18:12:52 by aourika           #+#    #+#             */
+/*   Updated: 2018/07/10 18:13:26 by aourika          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+void	ft_putchar(char c);
+
+void	display(int nb)
+{
+	int	first;
+	int	second;
+
+	if (nb > 9)
+	{
+		first = nb / 10;
+		second = nb % 10;
+		ft_putchar(first + 48);
+		ft_putchar(second + 48);
+	}
+	else
+	{
+		ft_putchar('0');
+		ft_putchar(nb + 48);
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	o;
+
+	i = 0;
+	while (i <= 99)
+	{
+		o = i + 1;
+		while (o <= 99)
+		{
+			display(i);
+			ft_putchar(' ');
+			display(o);
+			if (i < 98 || o < 99)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			++o;
+		}
+		++i;
+	}
+}
