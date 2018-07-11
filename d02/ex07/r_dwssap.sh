156f24e5866715d914bf63914e7fbfeda6b7962c
Abdeljalil OURIKA <aourika@e2r1p5.42.fr>
2018-07-11 16:19:22 +0200

    d02

--- d02/ex07/r_dwssap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cat /etc/passwd | grep -v '^#' | awk 'NR%2==0' | cut -d':' -f1 | rev | sort -r | head -n 16 | tail -n 14 | tr '
-' ',' | sed 's/,$/\./' | sed 's/,/, /g' | tr -d '
-' 
--- d02/ex08/bon.sh
+++
@@ -1,2 +0,0 @@
-ldapsearch -Q "(sn=*bon*)" 1.1 | grep "dn:" | wc -l | tr -d ' '
-
